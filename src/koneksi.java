7aca7ac45d6de1e91775f04347997f3599253e1c
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-10-29 14:46:20 +0700

    feat: add koneksi.java

---
+++ src/koneksi.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    private static Connection koneksi;
+    public static Connection getKoneksi() {
+        if (koneksi == null) {
+            try {
+                String url = new String();
+                String user = new String();
+                String password = new String();
+                url = "jdbc:mysql://localhost/rm-padang";
+                user = "root";
+                password = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                System.out.println("Gagal membuat koneksi: " + e.getMessage());
+            }
+        }
+        return koneksi;
+    }
+}
